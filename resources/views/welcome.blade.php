5b0ab32b63e1e8242c97480cd88773e91ddbe9b7
Abdus Satter <abdussatter51@gmail.com>
2020-01-31 01:38:59 +0600

    Change App Name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Welcome to SS Family
                 </div>
 
                 <div class="links">
